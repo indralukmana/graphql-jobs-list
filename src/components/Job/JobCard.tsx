b7a9f53b8428ce01fa1d3d60702e9729819439d8
indralukmana <indralukmana@live.com>
2022-01-14 16:11:35 +0700

    adjust stylings

--- src/components/Job/JobCard.tsx
+++ src/components/Job/JobCard.tsx
@@ -1,4 +1,13 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Badge,
+  Box,
+  Grid,
+  Heading,
+  HStack,
+  Text,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
 
 import { Jobs_jobs } from "queries/__generated__/Jobs";
 
@@ -8,13 +17,42 @@ interface JobCardProps {
 
 const JobCard = ({ job }: JobCardProps) => {
   return (
-    <Box borderWidth="1px" borderRadius="lg" p={5} overflow="hidden">
-      {job.title}
-      {job.company?.name}
-      {job.tags?.map((tag) => tag.name)}
-      {job.countries?.map((country) => country.name)}
-      {job.remotes?.map((remote) => remote.name)}
-    </Box>
+    <Grid
+      templateColumns={{ base: "1fr", md: "2fr 1fr 1fr" }}
+      gap={4}
+      borderWidth="1px"
+      borderRadius="lg"
+      p={5}
+      overflow="hidden"
+    >
+      <Box>
+        <Heading as="h3" size="md">
+          {job.title}
+        </Heading>
+        <Text as="h4" fontSize="md">
+          {job.company?.name}
+        </Text>
+      </Box>
+      <Wrap spacing={2}>
+        {job.tags?.map((tag) => (
+          <WrapItem>
+            <Badge>{tag.name}</Badge>
+          </WrapItem>
+        ))}
+      </Wrap>
+      <HStack spacing={2} maxW="md">
+        {job.countries?.map((country) => (
+          <Box borderWidth={1} p={1}>
+            {country.name}
+          </Box>
+        ))}
+        {job.remotes?.map((remote) => (
+          <Box borderWidth={1} p={1}>
+            {remote.name}
+          </Box>
+        ))}
+      </HStack>
+    </Grid>
   );
 };
 
